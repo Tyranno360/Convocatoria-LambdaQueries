748d75115050c12fc0b9868bd10b6c40591db9d0
Tyranno360 <elemental.hero360@mail.com>
2023-07-22 09:45:50 -0600

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+IEnumerable<string> query = names
+.Where(n => n.Contains("a"))
+.OrderBy(n => n.Length)
+.Select(n => n.ToUpper());
+foreach (var item in query)
+    Console.Write($"{item} |");
