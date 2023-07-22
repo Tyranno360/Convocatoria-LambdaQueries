f293429168b118b8d257b514c66b27b75f12737a
Tyranno360 <elemental.hero360@gmail.com>
2023-07-22 10:09:03 -0600

    se agrega el mensaje en el main

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿
+﻿//definicion de arreglo de cadenas 
 string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+//esta variable almancenara los resultados de filtro
 IEnumerable<string> query = names
+//filtro para obtener los nombres que contiene a 
 .Where(n => n.Contains("a"))
+//ordenados por el tamaño de caracteres
 .OrderBy(n => n.Length)
+//Convertir los seleccionados a mayuscula 
 .Select(n => n.ToUpper());
+//mostrar los nombres con criterios indicados
 foreach (var item in query)
     Console.Write($"{item} |");
